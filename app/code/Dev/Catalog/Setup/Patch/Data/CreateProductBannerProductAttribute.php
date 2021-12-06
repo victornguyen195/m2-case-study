000104ca41765f6d1ad7b0964d380c86519d3ecd
VinhNguyenHuy <vinhnh.uet.195@gmail.com>
2021-12-06 23:13:22 +0700

    Add product banner attribute

---
+++ app/code/Dev/Catalog/Setup/Patch/Data/CreateProductBannerProductAttribute.php
@@ -0,0 +1,85 @@
+<?php
+namespace Dev\Catalog\Setup\Patch\Data;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+class CreateProductBannerProductAttribute implements DataPatchInterface
+{
+    /** @var ModuleDataSetupInterface */
+    private $moduleDataSetup;
+
+    /** @var EavSetupFactory */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'product_banner',
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Product Banner',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => '',
+                'group' => 'Dev Banner',
+                'wysiwyg_enabled' => false,
+            ]
+        );
+
+//        $eavSetup->removeAttribute(Product::ENTITY, 'product_banner');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
