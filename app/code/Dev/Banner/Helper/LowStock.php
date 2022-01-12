f73522a6b47aa153a76d8ed45dec04cc8a4db7e3
VictorNguyen <vinhnh.uet.195@gmail.com>
2022-01-12 18:40:27 +0700

    Add low stock message

---
+++ app/code/Dev/Banner/Helper/LowStock.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dev\Banner\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class LowStock extends AbstractHelper
+{
+    const XML_PATH_LOW_STOCK = 'low-stock-config/general/low-stock-quantity';
+
+    /**
+     * @param string $field
+     * @param $storeId
+     * @return mixed
+     */
+    public function getLowStockConfigValue(string $field = self::XML_PATH_LOW_STOCK, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+}
---
+++ app/code/Dev/Banner/ViewModel/LowStock/LowStock.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Dev\Banner\ViewModel\LowStock;
+
+use Magento\Framework\Registry;
+use Dev\Banner\Helper\LowStock as LowStockHelper;
+use Magento\InventorySalesAdminUi\Model\GetSalableQuantityDataBySku;
+
+class LowStock implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    const OUT_OF_STOCK_MESSAGE = '在庫切れのため、お買い物カゴに投入できません。';
+    const LOW_STOCK_MESSAGE = '在庫が残りわずかです。';
+
+    /**
+     * @var Registry
+     */
+    protected $_registry;
+
+    /**
+     * @var LowStockHelper
+     */
+    protected $lowStockHelper;
+
+    /**
+     * @var GetSalableQuantityDataBySku
+     */
+    protected $getSalableQuantityDataBySku;
+
+
+    /**
+     * @param Registry $registry
+     * @param LowStockHelper $lowStockHelper
+     * @param GetSalableQuantityDataBySku $getSalableQuantityDataBySku
+     */
+    public function __construct(
+        Registry $registry,
+        LowStockHelper $lowStockHelper,
+        GetSalableQuantityDataBySku $getSalableQuantityDataBySku
+    ) {
+        $this->_registry = $registry;
+        $this->lowStockHelper = $lowStockHelper;
+        $this->getSalableQuantityDataBySku = $getSalableQuantityDataBySku;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLowStockMessageMessage(): string
+    {
+        $currentProduct = $this->_registry->registry('current_product');
+        $lowStockQuantity = $this->lowStockHelper->getLowStockConfigValue();
+        $salableQuantity = $this->getSalableQuantityDataBySku->execute($currentProduct->getSku());
+        $quantity = 0;
+        if (isset($salableQuantity[0]['qty']) && $salableQuantity[0]['qty']) {
+            $quantity = $salableQuantity[0]['qty'];
+        }
+
+        if (!$quantity) {
+            return self::OUT_OF_STOCK_MESSAGE;
+        }
+
+        if ($quantity < $lowStockQuantity) {
+            return self::LOW_STOCK_MESSAGE;
+        }
+
+        return '';
+    }
+}
