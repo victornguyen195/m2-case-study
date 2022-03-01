46d63feb783067128ab30276a0e566f451c85398
VinhNguyen <vinhnh.uet.195@gmail.com>
2022-03-01 21:59:13 +0700

    Layout, Block and Template

--- app/code/Dev/Banner/Controller/Index/View.php
+++ app/code/Dev/Banner/Controller/Index/View.php
@@ -6,29 +6,16 @@ class View extends \Magento\Framework\App\Action\Action
 {
     protected $_pageFactory;
 
-    protected $bannerRepository;
-
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory,
-        \Dev\Banner\Api\BannerRepositoryInterface $bannerRepository
+        \Magento\Framework\View\Result\PageFactory $pageFactory
     ) {
         $this->_pageFactory = $pageFactory;
-        $this->bannerRepository = $bannerRepository;
         return parent::__construct($context);
     }
 
     public function execute()
     {
-        $id = $this->_request->getParam('id');
-        $banner = $this->bannerRepository->getById($id);
-        var_dump($banner->getId());
-        var_dump($banner->getStatus());
-        var_dump($banner->getShortDescription());
-        var_dump($banner->getName());
-        var_dump($banner->getImage());
-
-        die;
         return $this->_pageFactory->create();
     }
 }
